7d8f68e0b35912bac35e8cd2c5898993a3745f03
kiosuper <32238626+kiosuper@users.noreply.github.com>
2019-03-05 15:34:37 +0700

    5/3/2019

---
+++ steg.py
@@ -0,0 +1,137 @@
+import sys
+
+
+def main():
+	#message to binary
+	def string_to_bin(mess):
+		l = []
+		for i in mess:
+			b = bin(ord(i))[2:]
+			if len(bin(ord(i))[2:]) < 8:
+				for i in range(8-len(bin(ord(i))[2:])):
+					b ='0' + b
+			for i in range(8):
+				l.append(int(b[i]))
+		return l
+
+
+	#encode
+	def encode(input_f, output_f, message_f):
+		f = open(input_f, 'rb')
+		l_in = f.read()
+		file_mess = open(message_f,'rb')
+		message = file_mess.read()
+		m = string_to_bin(message)
+		l_out = []
+
+		#copy 16 bytes header
+		for i in range(64):
+			l_out.append(l_in[i])
+
+		#add length message
+		l_out.append(chr(len(message)))
+
+		#add message
+		for i in range(65, len(l_in)):
+			if (i-65) < len(m): 
+				if ord(l_in[i]) % 2 == 0:
+					if m[i-65] == 1:
+						l_out.append(chr((ord(l_in[i]) + 1)%256))
+					else:
+						l_out.append(l_in[i])
+				else:
+					if m[i-65] == 0:
+						l_out.append(chr((ord(l_in[i]) + 1)%256))
+					else:
+						l_out.append(l_in[i])
+			else:
+				l_out.append(l_in[i])
+
+		#write to output file
+		o = open(output_f, 'wb')
+		for i in range(len(l_out)):
+			o.write(l_out[i])
+		print "Success!!!"
+
+
+	#binary to string
+	def bin_to_string(l):
+		a = []
+		for i in range(0,len(l),8):
+			s=''
+			for j in range(i, i+8):
+				s+=str(l[j])
+			s = '0b' + s
+			s = int(s,2)
+			a.append(s)
+		st = ''
+		for i in range(len(a)):
+			st+=chr(a[i])
+		return st
+
+
+	#decode
+	def decode(input_f,output_f):
+		f = open(input_f,'rb')
+		l_in = f.read()
+		l_out = []
+		len_mess = ord(l_in[64]) #check length message
+		for i in range(65, 65+len_mess*8):
+			if ord(l_in[i]) % 2 == 0:
+				l_out.append(0)
+			else:
+				l_out.append(1)
+		#write to ouput file
+		st = bin_to_string(l_out)
+		o = open(output_f,'wb')
+		o.write(st)
+
+	#Usage:
+	usage = '''Tool steganography to hiding message into audio using Parity method
+	Usage:
+		Encode:
+			-e: Encode
+			-i: Input file
+			-m: Message file
+			-o: Output file
+			Example: python steg.py -e -i Example1.wav -m message.txt -o Example2.wav
+		Decode:
+			-d: Decode
+			-i: Input file
+			-o: Message output file
+			Example: python steg.py -e -i Example1.wav -o message.txt
+'''
+	if len(sys.argv) > 1:
+		if sys.argv[1] == '-e':
+			input_f = ''
+			message_f = ''
+			output_f = ''
+			for i in range(2, len(sys.argv)):
+				if sys.argv[i] == '-i':
+					input_f = sys.argv[i+1]
+				elif sys.argv[i] == '-m':
+					message_f = sys.argv[i+1]
+				elif sys.argv[i] =='-o':
+					output_f = sys.argv[i+1]
+			if (input_f == '' or message_f == '' or output_f == ''):
+				print usage
+			else:
+				encode(input_f, output_f,message_f)
+		elif sys.argv[1] == '-d':
+			input_f = ''
+			output_f = ''
+			for i in range(2, len(sys.argv)):
+				if sys.argv[i] == '-i':
+					input_f = sys.argv[i+1]
+				elif sys.argv[i] =='-o':
+					output_f = sys.argv[i+1]
+			if (input_f == '' or output_f == ''):
+				print usage
+			else:
+				decode(input_f, output_f)
+	else:
+		print usage
+		
+
+if __name__ == '__main__':
+	main()
